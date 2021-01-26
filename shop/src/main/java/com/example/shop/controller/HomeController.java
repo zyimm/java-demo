cba828fcc609c134e7df24c7a8f3ac89d614aae8
zhouyangyang <zhouyangyang@hoge.cn>
2021-01-26 15:53:51 +0800

    shop

---
+++ shop/src/main/java/com/example/shop/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.example.shop.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+    public void index(){
+
+    }
+}
---
+++ shop/src/main/java/com/example/shop/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.example.shop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    public void productList(){
+
+    }
+}
