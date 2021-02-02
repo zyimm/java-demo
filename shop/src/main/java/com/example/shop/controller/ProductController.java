3b91255644a50696737ac10a6000e16d4aa5e0a8
zyimm <zyimm@qq.com>
2021-02-02 15:50:41 +0800

    商品详情

--- shop/src/main/java/com/example/shop/controller/ProductController.java
+++ shop/src/main/java/com/example/shop/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.shop.common.Result;
 import com.example.shop.service.product.impl.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,11 @@ public class ProductController {
     @PassToken(required = false)
     public Object productList(){
         return Result.success(this.productService.listProduct());
+    }
+
+    @GetMapping("detail/{id}")
+    @PassToken(required = false)
+    public Object productDetail(@PathVariable Integer id){
+        return Result.success(this.productService.getProductInfoById(id));
     }
 }
--- shop/src/main/java/com/example/shop/service/product/impl/ProductServiceImpl.java
+++ shop/src/main/java/com/example/shop/service/product/impl/ProductServiceImpl.java
@@ -40,4 +40,8 @@ public class ProductServiceImpl implements ProductService {
                 20L
         );
     }
+
+    public Product getProductDetail(Integer id){
+        return this.productMapper.selectById(id);
+    }
 }
