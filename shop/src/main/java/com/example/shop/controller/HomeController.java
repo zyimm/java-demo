040ba0b68a2ea263bfc84aa2f02236c9a47f1cf5
zyimm <zyimm@qq.com>
2021-03-12 09:16:51 +0800

    Hutool db test

--- shop/src/main/java/com/example/shop/controller/HomeController.java
+++ shop/src/main/java/com/example/shop/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.example.shop.controller;
 
+import cn.hutool.db.Db;
 import com.example.shop.annotation.PassToken;
 import com.example.shop.common.Result;
 import com.example.shop.entity.Member;
 import com.example.shop.entity.SystemCity;
 import com.example.shop.service.member.impl.MemberServiceImpl;
 import com.example.shop.service.system.impl.SystemCityServiceImpl;
+import com.example.shop.util.CustomIdGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +28,9 @@ public class HomeController {
 
     @Autowired
     SystemCityServiceImpl systemCityService;
+
+    @Autowired
+    CustomIdGenerator idGenerator;
 
     @Autowired
     public void setMemberService(MemberServiceImpl memberService){
@@ -48,4 +54,18 @@ public class HomeController {
         map.put("list",  cityList);
         return Result.success(map);
     }
+
+    @GetMapping("/test/uid")
+    @PassToken(required = false)
+    public Result testUId(){
+        return Result.success(idGenerator.nextId(systemCityService));
+    }
+
+    //
+    @GetMapping("/test/sql")
+    @PassToken(required = false)
+    public Result sql() throws SQLException {
+        return Result.success(Db.use().findAll("eb_user"));
+    }
+
 }
