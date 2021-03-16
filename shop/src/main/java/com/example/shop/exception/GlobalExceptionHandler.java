ab241d5507b7f8ac238678e8d2bd4895da3164fa
zyimm <zyimm@qq.com>
2021-03-16 09:13:02 +0800

    数据校验

--- shop/src/main/java/com/example/shop/exception/GlobalExceptionHandler.java
+++ shop/src/main/java/com/example/shop/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.shop.exception;
 
 import com.example.shop.common.Result;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,5 +24,20 @@ public class GlobalExceptionHandler {
             message = "服务器出错";
         }
         return Result.fail(message, e.getStackTrace());
+    }
+
+    /**
+     * handle 处理
+     *
+     * @param e 异常
+     * @return Result
+     */
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Result MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        // 从异常对象中拿到ObjectError对象
+        ObjectError objectError = e.getBindingResult().getAllErrors().get(0);
+        // 然后提取错误提示信息进行返回
+        return Result.fail(objectError.getDefaultMessage(), objectError.getArguments());
     }
 }
